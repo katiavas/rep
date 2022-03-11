c10ef523bc47dac60d61e2ba50465919353b8c4b
katiavas <72811416+katiavas@users.noreply.github.com>
2022-03-11 09:45:15 +0000

    Update wrappers.py

--- intrinsic/wrappers.py
+++ intrinsic/wrappers.py
@@ -1,39 +1,8 @@
-import collections
-import cv2
-import numpy as np
-import gym
-
 
 import collections
 import cv2
 import numpy as np
 import gym
-import gym_miniworld
-
-
-class RepeatAction(gym.Wrapper):
-    def __init__(self, env=None, repeat=4, fire_first=False):
-        super(RepeatAction, self).__init__(env)
-        self.repeat = repeat
-        self.shape = env.observation_space.low.shape
-        self.fire_first = fire_first
-
-    def step(self, action):
-        t_reward = 0.0
-        done = False
-        for i in range(self.repeat):
-            obs, reward, done, info = self.env.step(action)
-            t_reward += reward
-            if done:
-                break
-        return obs, t_reward, done, info
-
-    def reset(self):
-        obs = self.env.reset()
-        if self.fire_first:
-            assert self.env.unwrapped.get_action_meanings()[1] == 'FIRE'
-            obs, _, _, _ = self.env_step(1)
-        return obs
 
 
 class PreprocessFrame(gym.ObservationWrapper):
@@ -54,32 +23,9 @@ class PreprocessFrame(gym.ObservationWrapper):
         return new_obs
 
 
-class StackFrames(gym.ObservationWrapper):
-    def __init__(self, env, repeat):
-        super(StackFrames, self).__init__(env)
-        self.observation_space = gym.spaces.Box(
-                env.observation_space.low.repeat(repeat, axis=0),
-                env.observation_space.high.repeat(repeat, axis=0),
-                dtype=np.float32)
-        self.stack = collections.deque(maxlen=repeat)
-
-    def reset(self):
-        self.stack.clear()
-        observation = self.env.reset()
-        for _ in range(self.stack.maxlen):
-            self.stack.append(observation)
-
-        return np.array(self.stack).reshape(self.observation_space.low.shape)
-
-    def observation(self, observation):
-        self.stack.append(observation)
-
-        return np.array(self.stack).reshape(self.observation_space.low.shape)
 
 
-def make_env(env_name, shape=(42, 42, 1), repeat=4):
+def make_env(env_name, shape=(42, 42, 1)):
     env = gym.make(env_name)
-    env = RepeatAction(env, repeat)
     env = PreprocessFrame(shape, env)
-    env = StackFrames(env, repeat)
     return env

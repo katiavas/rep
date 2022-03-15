bad3c9d3cf6f01541b2bc278f63eaf1f4fe8332c
katiavas <bl19548@bristol.ac.uk>
2022-03-15 19:41:46 +0000

    stack 4 frames to get velocity

--- intrinsic/wrappers.py
+++ intrinsic/wrappers.py
@@ -62,7 +62,7 @@ class PreprocessFrame(gym.ObservationWrapper):
         self.observation_space = gym.spaces.Box(low=0.0, high=1.0,
                                                 shape=self.shape,
                                                 dtype=np.float32)
-    '''
+
     def observation(self, obs):
         new_frame = cv2.cvtColor(obs, cv2.COLOR_RGB2GRAY)
         resized_screen = cv2.resize(new_frame, self.shape[1:],
@@ -70,8 +70,8 @@ class PreprocessFrame(gym.ObservationWrapper):
         new_obs = np.array(resized_screen, dtype=np.uint8).reshape(self.shape)
         new_obs = new_obs / 255.0
 
-        return new_obs'''
-
+        return new_obs
+    '''
     def observation(self, obs):
         return PreprocessFrame.process(obs)
 
@@ -80,7 +80,7 @@ class PreprocessFrame(gym.ObservationWrapper):
         resized_screen = cv2.resize(new_frame, self.shape[1:], interpolation=cv2.INTER_AREA)
         new_obs = np.array(resized_screen, dtype=np.uint8).reshape(self.shape)
         new_obs = new_obs / 255.0
-        return new_obs
+        return new_obs'''
 
 
 class StackFrames(gym.ObservationWrapper):

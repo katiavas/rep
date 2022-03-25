9b4c63a28e1fcdaf02db9d67ff28e07b0d915b49
katiavas <bl19548@bristol.ac.uk>
2022-03-25 19:45:52 +0000

    CHANGES

--- intrinsic/icm.py
+++ intrinsic/icm.py
@@ -4,7 +4,7 @@ import torch.nn.functional as F
 
 
 class Encoder(nn.Module):
-    '''def __init__(self, input_dims, feature_dim=288):
+    def __init__(self, input_dims, feature_dim=288):
         super(Encoder, self).__init__()
         
         self.conv1 = nn.Conv2d(input_dims[0], 32, (3, 3), stride=2, padding=1)
@@ -20,9 +20,9 @@ class Encoder(nn.Module):
         # [T, 32, 3, 3] to [T, 288]
         enc_flatten = T.flatten(enc, start_dim=1)
         # conv = enc.view(enc.size()[0], -1).to(T.float)
-        return enc_flatten'''
+        return enc_flatten
 
-    def __init__(self, input_dims, feature_dim=288):
+    '''def __init__(self, input_dims, feature_dim=288):
         super(Encoder, self).__init__()
 
         self.conv1 = nn.Conv2d(input_dims[0], 32, (3, 3), stride=2, padding=1)
@@ -54,7 +54,7 @@ class Encoder(nn.Module):
         # conv = enc.view(enc.size()[0], -1).to(T.float)
         features = self.fc1(enc_flatten)
 
-        return features
+        return features'''
 
 
 class ICM(nn.Module):

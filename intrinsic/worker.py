9811e5bae235a8c81c87b45f7a1a617871ab6d61
katiavas <bl19548@bristol.ac.uk>
2022-03-28 11:48:15 +0100

    save

--- intrinsic/worker.py
+++ intrinsic/worker.py
@@ -21,8 +21,8 @@ def worker(name, input_shape, n_actions, global_agent,
         print(local_agent)
     else:
         local_agent = ActorCritic(input_shape, n_actions)
-        loc = local_agent.save_models(input_dims=input_shape, n_actions=n_actions)
-        print(loc)
+        # loc = local_agent.save_models(input_dims=input_shape, n_actions=n_actions)
+        # print(loc)
     # local_agent = ActorCritic(input_shape, n_actions)
 
     if icm:
@@ -81,6 +81,7 @@ def worker(name, input_shape, n_actions, global_agent,
                 optimizer.step()
                 # local_agent.save('actor')
                 local_agent.load_state_dict(global_agent.state_dict())
+                print(local_agent)
 
 
                 if icm:

157520b1a253d69afca12e778b08608d84f4fff1
katiavas <bl19548@bristol.ac.uk>
2022-03-29 10:44:25 +0100

    save

--- intrinsic/worker.py
+++ intrinsic/worker.py
@@ -8,13 +8,15 @@ from wrappers import make_atari
 # import torchvision.models as models
 import random
 from utils import plot_learning_curve_with_shaded_error
+
+
 # import wandb
 
 
 def worker(name, input_shape, n_actions, global_agent,
            optimizer, env_id, n_threads, global_idx, global_icm,
            icm_optimizer, icm):
-    LOAD = True
+    LOAD = False
     frame_buffer = [input_shape[1], input_shape[2], 1]
     env = make_atari(env_id, shape=frame_buffer)
     T_MAX = 20
@@ -23,7 +25,6 @@ def worker(name, input_shape, n_actions, global_agent,
     random.seed(SEED)
     np.random.seed(SEED)
     T.manual_seed(SEED)
-
 
     if LOAD:
         # local_agent = ActorCritic(input_shape, n_actions)
@@ -47,7 +48,7 @@ def worker(name, input_shape, n_actions, global_agent,
             # local_icm.load_state_dict(T.load('icm_weights.pth'))
             local_icm = T.load('icm_weights1.pt')
             local_icm.eval()
-            #with T.no_grad():
+            # with T.no_grad():
             #   local_icm(ICM(input_shape, n_actions))
         else:
             local_icm = ICM(input_shape, n_actions)
@@ -83,10 +84,10 @@ def worker(name, input_shape, n_actions, global_agent,
             t_steps += 1
             if ep_steps % T_MAX == 0 or done:
                 states, actions, new_states, rewards, values, log_probs = \
-                        memory.sample_memory()
+                    memory.sample_memory()
                 if icm:
                     intrinsic_reward, L_I, L_F = \
-                            local_icm.calc_loss(states, new_states, actions)
+                        local_icm.calc_loss(states, new_states, actions)
                     # wandb.log({'forward_loss':L_F.item(), 'inverse_loss':L_I.item(), 'intrinsic_reward': intrinsic_reward})
                 loss = local_agent.calc_loss(obs, hx, done, rewards,
                                              values, log_probs,
@@ -99,8 +100,8 @@ def worker(name, input_shape, n_actions, global_agent,
                 loss.backward()
                 T.nn.utils.clip_grad_norm_(local_agent.parameters(), 40)
                 for local_param, global_param in zip(
-                                        local_agent.parameters(),
-                                        global_agent.parameters()):
+                        local_agent.parameters(),
+                        global_agent.parameters()):
                     global_param._grad = local_param.grad
                 optimizer.step()
                 # local_agent.save('actor')
@@ -108,8 +109,8 @@ def worker(name, input_shape, n_actions, global_agent,
 
                 if icm:
                     for local_param, global_param in zip(
-                                        local_icm.parameters(),
-                                        global_icm.parameters()):
+                            local_icm.parameters(),
+                            global_icm.parameters()):
                         global_param._grad = local_param.grad
                     icm_optimizer.step()
                     local_icm.load_state_dict(global_icm.state_dict())
@@ -120,9 +121,10 @@ def worker(name, input_shape, n_actions, global_agent,
         # with global_idx.get_lock():
         #    global_idx.value += 1
         if name == '1':
-            T.save(local_agent, 'actor_weights1.pt')
-            if icm:
-                T.save(local_icm, 'icm_weights1.pt')
+            if (LOAD == False):
+                T.save(local_agent, 'actor_weights1.pt')
+                if icm:
+                    T.save(local_icm, 'icm_weights1.pt')
             # loss_i = T.sum(L_I)
             # l_i.append(loss_i)
             # loss_f = T.sum(L_F)
@@ -133,12 +135,12 @@ def worker(name, input_shape, n_actions, global_agent,
             # intr.append(a.detach().numpy())  # for plotting intrinsic reward
             scores.append(score)
             avg_score = np.mean(scores[-100:])
-            avg_score_5000 = np.mean(scores[max(0, episode-5000): episode+1])
+            avg_score_5000 = np.mean(scores[max(0, episode - 5000): episode + 1])
             print('ICM episode {} thread {} of {} steps {:.2f}M score {:.2f} '
                   'avg score (100) {:.2f}'.format(
-                                                episode, name, n_threads,
-                                                t_steps/1e6, score,
-                                                avg_score))
+                episode, name, n_threads,
+                t_steps / 1e6, score,
+                avg_score))
     if name == '1':
         x = [z for z in range(episode)]
         # plot_learning_curve(x, scores, 'Cartpole_pixels_ICM.png')

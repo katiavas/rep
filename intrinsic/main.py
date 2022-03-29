393423004636e1afc2bb2cf9a664b3370025fd4b
katiavas <bl19548@bristol.ac.uk>
2022-03-29 20:26:26 +0100

    save

--- intrinsic/main.py
+++ intrinsic/main.py
@@ -14,11 +14,17 @@ os.environ['OMP_NUM_THREADS'] = '1'
 # wandb.init(project='icm', entity="katiavas", dir='./')
 
 if __name__ == '__main__':
+    SEED = 111
     env_id = 'ALE/Breakout-v5'
+    random.seed(SEED)
+    np.random.seed(SEED)
+    T.manual_seed(SEED)
+    T.cuda.manual_seed(SEED)
     # gym_env = gym.make(env_id)
     # gym_env.seed(SEED)
     # gym_env.action_space.seed(SEED)
     # gym_env.observation_space.seed(SEED)
+    T.use_deterministic_algorithms(True)
     mp.set_start_method('spawn', force=True)
     global_ep = mp.Value('i', 0)
     # env_id = 'PongNoFrameskip-v4'

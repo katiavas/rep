2e842bd559a7ebc0e9b4a7b9d7dc9057371f1d8d
katiavas <bl19548@bristol.ac.uk>
2022-03-29 16:44:34 +0100

    save

--- intrinsic/icm.py
+++ intrinsic/icm.py
@@ -6,7 +6,7 @@ import os
 
 
 class Encoder(nn.Module):
-    def __init__(self, input_dims, feature_dim=288):
+    '''def __init__(self, input_dims, feature_dim=288):
         super(Encoder, self).__init__()
 
         self.conv1 = nn.Conv2d(input_dims[0], 32, (3, 3), stride=2, padding=1)
@@ -22,9 +22,9 @@ class Encoder(nn.Module):
         # [T, 32, 3, 3] to [T, 288]
         enc_flatten = T.flatten(enc, start_dim=1)
         # conv = enc.view(enc.size()[0], -1).to(T.float)
-        return enc_flatten
+        return enc_flatten'''
 
-    '''def __init__(self, input_dims, feature_dim=288):
+    def __init__(self, input_dims, feature_dim=288):
         super(Encoder, self).__init__()
 
         self.conv1 = nn.Conv2d(input_dims[0], 32, (3, 3), stride=2, padding=1)
@@ -56,7 +56,7 @@ class Encoder(nn.Module):
         # conv = enc.view(enc.size()[0], -1).to(T.float)
         features = self.fc1(enc_flatten)
 
-        return features'''
+        return features
 
 
 class ICM(nn.Module):

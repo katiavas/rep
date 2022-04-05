280723b80347b954d40fa7349415f01aece30dc5
katiavas <bl19548@bristol.ac.uk>
2022-04-05 22:57:03 +0100

    save

--- intrinsic/icm.py
+++ intrinsic/icm.py
@@ -13,7 +13,7 @@ This is a cross entropy loss between the predicted action and the actual action 
 
 
 class Encoder(nn.Module):
-    '''def __init__(self, input_dims, feature_dim=288):
+    def __init__(self, input_dims, feature_dim=288):
         super(Encoder, self).__init__()
 
         self.conv1 = nn.Conv2d(input_dims[0], 32, (3, 3), stride=2, padding=1)
@@ -29,9 +29,9 @@ class Encoder(nn.Module):
         # [T, 32, 3, 3] to [T, 288]
         enc_flatten = T.flatten(enc, start_dim=1)
         # conv = enc.view(enc.size()[0], -1).to(T.float)
-        return enc_flatten'''
+        return enc_flatten
 
-    def __init__(self, input_dims, feature_dim=288):
+    '''def __init__(self, input_dims, feature_dim=288):
         super(Encoder, self).__init__()
 
         self.conv1 = nn.Conv2d(input_dims[0], 32, (3, 3), stride=2, padding=1)
@@ -63,7 +63,7 @@ class Encoder(nn.Module):
         # conv = enc.view(enc.size()[0], -1).to(T.float)
         features = self.fc1(enc_flatten)
 
-        return features
+        return features'''
 
 
 class ICM(nn.Module):

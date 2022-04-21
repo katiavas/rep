b9f2f77b67679adc789d55bf6594a06f00c12988
katiavas <bl19548@bristol.ac.uk>
2022-04-21 01:44:45 +0100

    save

--- intrinsic/worker.py
+++ intrinsic/worker.py
@@ -67,7 +67,7 @@ def worker(name, input_shape, n_actions, global_agent,
     # frame_buffer = [input_shape[1], input_shape[2], 1]
     # env = make_atari(env_id, shape=frame_buffer)
 
-    episode, max_steps, t_steps, scores = 0, 10000, 0, []
+    episode, max_steps, t_steps, scores = 0, 5000, 0, []
     intr = []
     l = []
     l_i = []
